e4ccb043dd07b0f54ae9428bbcbe42c7e46ab672
Diogo Lima <diogo.ll.lima08@gmail.com>
2023-09-23 22:20:51 -0300

    Algoritmo Guloso e MergeSort

--- Guloso/Guloso.py
+++ Guloso/Guloso.py
@@ -1,6 +1,6 @@
 
 def mochilafracionaria(p, v, n, c):
-    x = [0] * (n + 1)  # Inicializa a lista x com zeros para representar as frações
+    x = [] 
     for i in range(1, n + 1):
         if p[i] <= c:
             x[i] = 1
